229292bc2202cd88d288e35c508b6d6ee008896f
vietnq-2410 <nguyen.quoc.viet-b@sun-asterisk.com>
2021-01-14 16:54:03 +0700

    Chapter 12: Reset password

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,8 +4,8 @@ class UserMailer < ApplicationMailer
     mail to: @user.email, subject: t("mail.acc_activation")
   end
 
-  def password_reset
-    @greeting = "Hi"
-    mail to: "to@example.org"
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: t("account.reset_pass_subject")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     post "/signup", to: "users#create"
     resources :users
     resources :account_activations, only: %i(edit)
+    resources :password_resets, except: %i(index show destroy)
     get "/login", to: "session#new"
     post "/login", to: "session#create"
     delete "logout", to: "session#destroy"
